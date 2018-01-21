7cf75f5b265e1ca7f1fa21510582a2de667d7213
styczynski <piotrsty1@gmail.com>
2018-01-21 17:54:58 +0100

    Change pipes.c example

--- examples/pipes.c
+++ examples/pipes.c
@@ -3,76 +3,7 @@
 #include "fork.h"
 #include "memalloc.h"
 #include "syslog.h"
-#include "gcinit.h"
 
-int main(void) {
-    
-    GC_SETUP();
-    GC_LOG_ON();
-    
-    char* data = MALLOCATE_ARRAY(char, 3);
-    data[0] = 13;
-    printf("Root: Data is %p = %d\n", (void*) data, data[0]);
-    
-    
-    pid_t pid;
-    int status = processFork(&pid);
-    
-    if(status == 1) {
-        data[0] = 42;
-        printf("Child: Data is %p = %d\n", (void*) data, data[0]);
-        int j = 0;
-        for(int i=0; i<100; ++i) {
-            if(j%2) ++j; else --j;
-        }
-        printf("Child: Data is %p = %d\n", (void*) data, data[0]);
-    } else if (status == 0) {
-        data[0] = 18;
-        printf("Parent: Data is %p = %d\n", (void*) data, data[0]);
-        int j = 0;
-        for(int i=0; i<100; ++i) {
-            if(j%2) ++j; else --j;
-        }
-        printf("Parent: Data is %p = %d\n", (void*) data, data[0]);
-    }
-    
-    processWaitForAll();
-    FREE(data);
-    return 0;
-    
-}
-
-
-/*include "hashmap.h"
-
-typedef struct custom_type custom_type;
-struct custom_type {
-    int x;
-    int y;
-};
-
-int main(void) {
-    HashMap hm = HashMapNew(HashMapIntCmp);
-    
-    custom_type val;
-    val.x = 21;
-    val.y = 37;
-    
-    HashMapSetV(hm, int, custom_type, 3, val);
-    
-    val.x = 1;
-    val.y = 2;
-    HashMapSetV(hm, int, custom_type, 4, val);
-    
-    LOOP_HASHMAP(&hm, i) {
-        custom_type* ptr = (custom_type*) HashMapGetValue(i);
-        printf("custom_type x=%d, y=%d\n", ptr->x, ptr->y);
-    }
-    
-    HashMapDestroyV(hm, int, custom_type);
-}*/
-
-/*
 int main(void) {
     
     MsgPipeID taskPipeID = msgPipeCreate(100);
@@ -104,5 +35,5 @@ int main(void) {
     }
     
     return 0;
-}*/
+}
 
